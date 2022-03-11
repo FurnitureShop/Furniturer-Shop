385f6a61b6ce09b85346b2825d3184e291ee231f
bao <bao200118@gmail.com>
2022-03-11 20:36:02 +0700

    Add config antd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './App.less';
+import {Button} from 'antd'
 
 function App() {
   return (
     <div className="App">
-      
+      <Button>aBC</Button>
     </div>
   );
 }
