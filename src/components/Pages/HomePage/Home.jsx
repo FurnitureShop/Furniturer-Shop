6aec0545f1f94d98e360ca506f45dd3b9291532b
PhamneyuT <koduapass@gmail.com>
2022-03-18 17:52:27 +0700

    update Navbar

--- src/components/Pages/HomePage/Home.jsx
+++ src/components/Pages/HomePage/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>
+        <div style={{backgroundColor: "red"}}>
             <h1>This is a home page</h1>
         </div>
     );
