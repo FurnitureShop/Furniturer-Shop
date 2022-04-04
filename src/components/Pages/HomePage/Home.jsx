3f64dde16d273c074849b721551ff45019cae3e2
PhamneyuT <koduapass@gmail.com>
2022-04-04 22:34:51 +0700

    working on the LastestItems

--- src/components/Pages/HomePage/Home.jsx
+++ src/components/Pages/HomePage/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
+
 import HeroSlider from "./HeroSlider/HeroSlider";
 import CoverImages from "./CoverImages/CoverImages";
 import OurServices from "./OurServices/OurServices";
+import LastestItems from "./LastestItems/LastestItems";
 
 const Home = () => {
     return (
         <div>
             <h1>This is a home page</h1>
             <HeroSlider />
+            <LastestItems />
             <OurServices />
             <CoverImages />
         </div>
---
+++ src/components/Product/ProductItem/ProductItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./ProductItem.scss"
+
+const ProductItem = () => {
+    return ( 
+        <div className='product'>
+            <div className='product__thumbnail'>
+
+            </div>
+            <div className='product__info'>
+                <h5>Chair demo</h5>
+            </div>
+        </div>
+    )
+}
+
+export default ProductItem;
