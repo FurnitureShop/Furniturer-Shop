9f81a1dc60f9886ac3fac2387fea4398db7f4598
PhamneyuT <koduapass@gmail.com>
2022-04-24 18:02:53 +0700

    //update

--- src/components/Pages/ProductPage/Filters/FilterPanel.jsx
+++ src/components/Pages/ProductPage/Filters/FilterPanel.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import FilterCategory from "./FilterCategory/FilterCategory";
 import "./FilterPanel.scss"
 import FilterSearch from "./FilterSearch/FilterSearch";
 import FilterSort from "./FilterSort/FilterSort";
 
 const FilterPanel = () => {
     return (
-        <section>
-            <div>
+        <aside>
+            <div className="filter-panel flex flex-col gap-y-7">
                 <FilterSearch />
                 <FilterSort />
-                
+                <FilterCategory />
             </div>
-        </section>
+        </aside>
     )
 }
 
--- src/components/Pages/ProductPage/Filters/FilterSort/FilterSort.jsx
+++ src/components/Pages/ProductPage/Filters/FilterSort/FilterSort.jsx
@@ -1,16 +1,28 @@
-import { Form } from "antd";
+import { Form, Select } from "antd";
 import React from "react";
 import "./FilterSort.scss"
 
+const { Option } = Select;
 
 const FilterSort = () => {
 
     return (
         <div className="filter-by-sort">
             <h4>Sorting</h4>
-            <Form>
-                
-            </Form>
+            <div className="filter-by-sort_content">
+                <Form>
+                    <Select
+                        defaultValue={"default"}
+                        bordered={false}
+                        size={"large"}
+                        style={{width: "100%"}}>
+                        <Option value="default">Default Sorting</Option>
+                        <Option value="latest">Sort by latest</Option>
+                        <Option value="lowhigh">Sort by price: low to high</Option>
+                        <Option value="highlow">Sort by price: high to low</Option>
+                    </Select>
+                </Form>
+            </div>
         </div>
     )
 }
