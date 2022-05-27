00d2482fe586984572aa7aaa695a328cf83ccd7c
PhamneyuT <koduapass@gmail.com>
2022-05-27 15:48:06 +0700

    fixed the imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.less';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import RoutePaths from './paths/Routes';
-import MainLayout from 'components/MainLayout';
+import RoutePaths from './Paths/Routes';
+//import MainLayout from 'components/MainLayout';
 import "./App.scss"
 
 function App() {
