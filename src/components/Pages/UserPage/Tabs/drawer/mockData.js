e6b88fd05b6158578259b2078969df2025ba68ec
bao200118 <60221952+bao200118@users.noreply.github.com>
2022-05-30 21:35:18 +0700

    add order tracking function

---
+++ src/components/Pages/UserPage/Tabs/drawer/mockData.js
@@ -0,0 +1,25 @@
+export const data = [{
+    _id: "1234",
+    customerID: "012345",
+    customerName: "Pham Gia Bao",
+    phone: "0123456789",
+    address: "HELL",
+    products: [
+        {
+            //Product ID
+            product: "625ae0370209ff47e2488440",
+            quantity: 1,
+        },
+        {
+            //Product ID
+            product: "625b9e984ddaf5fc57c812ad",
+            quantity: 2,
+        },
+    ],
+    totalPrice: "140",
+    isPaid: true,
+    paymentMethod: "Cash",
+    status: "Create order",
+    note: '',
+    createAt: "2022-04-16T15:26:47.923Z",
+}]
