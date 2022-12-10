7c71e1ec095d0315e7fd48f960255857f9af2f5e
bao200118 <60221952+bao200118@users.noreply.github.com>
2022-12-10 09:48:59 +0000

    Update chatbot name

--- src/App.js
+++ src/App.js
@@ -8,32 +8,32 @@ import LocalStorageService from "services/LocalStorage";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 function App() {
-  useEffect(() => {
-    return () => {
-      LocalStorageService.clearToken("auth");
-      LocalStorageService.clearToken("refresh");
-    };
-  });
-  return (
-    <PayPalScriptProvider
-      options={{
-        "client-id":
-          "AUB4X1GoVsxKeN6iiCzHxQmt5EPGX_1PVJLep-CP325v4jXNxXvtjvZ16gGdITnC_dvVR89Exb4Bz8LL",
-        components: "buttons",
-        currency: "USD",
-      }}
-    >
-      <BrowserRouter>
-        <RoutePaths />
-        <df-messenger
-          intent="WELCOME"
-          chat-title="Đây_là_1_con_bot"
-          agent-id="9917ee40-d9df-4117-8e97-b1affcc6a5cf"
-          language-code="en"
-        ></df-messenger>
-      </BrowserRouter>
-    </PayPalScriptProvider>
-  );
+	useEffect(() => {
+		return () => {
+			LocalStorageService.clearToken("auth");
+			LocalStorageService.clearToken("refresh");
+		};
+	});
+	return (
+		<PayPalScriptProvider
+			options={{
+				"client-id":
+					"AUB4X1GoVsxKeN6iiCzHxQmt5EPGX_1PVJLep-CP325v4jXNxXvtjvZ16gGdITnC_dvVR89Exb4Bz8LL",
+				components: "buttons",
+				currency: "USD",
+			}}
+		>
+			<BrowserRouter>
+				<RoutePaths />
+				<df-messenger
+					intent="WELCOME"
+					chat-title="Furniturer shop"
+					agent-id="9917ee40-d9df-4117-8e97-b1affcc6a5cf"
+					language-code="en"
+				></df-messenger>
+			</BrowserRouter>
+		</PayPalScriptProvider>
+	);
 }
 
 export default App;
